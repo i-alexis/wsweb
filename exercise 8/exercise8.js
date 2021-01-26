f53196bb2b53cff767bb940d0ed5a10ae8dc2723
i-alexis <77300748+i-alexis@users.noreply.github.com>
2021-01-26 17:48:48 -0500

    exercises

---
+++ exercise 8/exercise8.js
@@ -0,0 +1,5 @@
+let myButton=document.createElement(“BUTTON”);
+let myExistingDiv=document.getElementById(button1);
+myExistingDiv.appendChild(myButton);
+myExistingDiv.innerHTML="hello"
+myExistingDiv.style.backgroundColor="black";
